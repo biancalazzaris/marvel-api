9dd79ae4a0d92e64ae7add7688740d61f5f1ed69
Bianca Lazzaris <61711182+biancalazzaris@users.noreply.github.com>
2022-11-16 21:39:08 -0300

    Ajustando o get_data

--- main.py
+++ main.py
@@ -8,68 +8,47 @@ from pyspark.sql.types import *
 
 timestamp = dt.datetime.now().strftime('%Y-%m-%d%H:%M:%S')
 
-class MarvelApi():
+class marvel_series():
 
-    def __init__(self, route: str) -> None:
-        self.route = route
-        self.base_endpoint = "https://gateway.marvel.com:443/v1/public/"
+    def __init__(self):
+        self.base_endpoint = "https://gateway.marvel.com:443/v1/public/series"
 
-    def params_api():        
+    def params_api(self):        
         hash_md5 = hashlib.md5()
         hash_md5.update(f'{timestamp}{PRIVATE_KEY}{PUBLIC_KEY}'.encode('utf-8'))
         hashed_params = hash_md5.hexdigest()
 
         return hashed_params
             
-    def get_data(self, **kwargs):
-        endpoint = self.base_endpoint(**kwargs)
+    def get_data(self):
+        endpoint = self.base_endpoint
         params = {'ts': timestamp, 'apikey': PUBLIC_KEY, 'hash': self.params_api(), 'offset': 0, 'limit':100}
-        response = requests.get(endpoint, self.route, params=params)
+        response = requests.get(endpoint,  params=params)
         return response.json()
-        
 
 
 
 
-# for i in range(len(res["data"]["results"])):
-#     print(res["data"]["results"][i]["id"],
-#         res["data"]["results"][i]["name"])
+objeto = marvel_series()
+print(objeto.get_data(0))
 
 
 
-""" Construindo o Data Frame """
-"""
-table_name = 'series'
-results = pd.DataFrame(response["data"]["results"])
-# results.to_json('series.json')
-
-spark = SparkSession.builder.appName("demo-app").getOrCreate()
-df = spark.read.json("./series.json")
-
-df.createOrReplaceTempView(table_name)
-
-df_sql = spark.sql("SELECT * FROM series").show()
-# df.show()
- """
+#  """ Construindo o Data Frame """
+# table_name = 'series'
+# results = pd.DataFrame(response["data"]["results"])
+# # results.to_json('series.json')
+# spark = SparkSession.builder.appName("demo-app").getOrCreate()
+# df = spark.read.json("./series.json")
+# df.createOrReplaceTempView(table_name)
+# df_sql = spark.sql("SELECT * FROM series").show()
+# # df.show()
 
 
-"""Conectando com o banco MYSQL"""
-
-
+# """Conectando com o banco MYSQL"""
 # engine = create_engine('mysql+://root:password@localhost:3306/db') #change to connect your mysql
 # #if you want to append the data to an existing table
 # # df.to_sql(name='SQL Table name',con=engine,if_exists='append',index=False) 
 
 # #if you want to create a new table 
 # df.to_sql(name='series',con=engine,if_exists='fail',index=False) 
-
-
-""" try:
-    db = connection.connect(host="localhost", database = 'db',user="root", passwd="password",use_pure=True)
-    query = "CREATE TABLE IF NOT EXISTS  SERIES"
-    df = pd.read_sql(query,db)
-    db.close() #close the connection
-
-except Exception as e:
-    db.close()
-    print(str(e)) """
